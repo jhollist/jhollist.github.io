ead24ae7906677de0a74ec63765b9e454c96acb0
Jeffrey W. Holllister <jeff.w.hollister@gmail.com>
2018-01-14 09:16:00 -0500

    pull current version working???

--- _rmd_posts/pull_curr_version.R
+++ _rmd_posts/pull_curr_version.R
@@ -3,5 +3,17 @@ library(dplyr)
 library(stringr)
 url <- 'https://download2.rstudio.org'
 dat <- read_xml(url)
-dat_l <- as(xml_children(dat), "list")
-xml_text(dat_l[[6]])
+dat_txt <- xml_text(xml_children(dat)) %>%
+  tbl_df
+curr_vers_date <- dat_txt %>%
+  filter(str_detect(.$value,"current")) %>%
+  str_sub(12,21)
+deb_file<-dat_txt %>%
+  filter(str_detect(.$value,curr_vers_date)) %>%
+  filter(str_detect(.$value,"amd64.deb")) %>%
+  filter(!str_detect(.$value,"pro")) %>%
+  str_sub(1,str_locate(.$value,curr_vers_date)-1)
+deb_file <- deb_file[1]
+  
+
+
